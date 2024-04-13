35ab0bdb76ac27e7faed219890770e155de5d2c5
xochitljohnson <95721992+xochitljohnson@users.noreply.github.com>
2024-04-13 10:29:59 -0700

    added comments

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // App initialization
+const PORT = 3000;
 const express = require('express');
 const app = express();
 
+// Import and init routes
 const transit_routes = require('./transit_routes_router/index')
-
-const PORT = 3000;
 
 app.use('/routes', transit_routes)
 
+// Serve landing page
 app.get('/', (req, res) => res.send('Hello world')).listen(PORT)
 
+// Obligatory console log
 console.log(`Listening on port: ${PORT}`)
