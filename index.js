36a4d3f5ef1f33c195aa227907a8688514be0c8a
xochitljohnson <95721992+xochitljohnson@users.noreply.github.com>
2024-04-13 17:16:06 -0700

    added placeholder docs for API

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // App initialization
 const PORT = 3000;
+const path = require('path');
 const express = require('express');
 const app = express();
 
 // Import and init routes
-const transit_routes = require('./transit_routes_router/index')
+const transit_routes = require('./transit_routes_router/index');
+
+app.use(express.static(path.join(__dirname,'/public')))
 
 app.use('/routes', transit_routes)
 
 // Serve landing page
-app.get('/', (req, res) => res.render('index', {}))
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(PORT)
 
