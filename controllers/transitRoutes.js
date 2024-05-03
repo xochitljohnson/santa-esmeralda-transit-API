98c0d213955c6729d7dbb078c1ac51ffcddcca06
xochitljohnson <95721992+xochitljohnson@users.noreply.github.com>
2024-05-02 21:27:46 -0400

    updated controllers

--- controllers/transitRoutes.js
+++ controllers/transitRoutes.js
@@ -1,34 +1,61 @@
+const TransitRouteSchema = require('../schemas/transitRouteSchema')
+
 // @desc Get all transit routes
 // @route GET /api/v1/transit-routes
 // @access Public
-exports.getAllTransitRoutes = (req, res, next) => {
-    res.status(200).json({success:true, message:"All transit routes"})
+exports.getAllTransitRoutes = async (req, res, next) => {
+  try {
+    const transitRoutes = await TransitRouteSchema.find(req.body)
+    res.status(200).json({data: transitRoutes})
+  } catch (error) {
+    res.status(400)
+  }
 }
 
 // @desc Get single transit route by ID
 // @route GET /api/v1/transit-routes/:id
 // @access Public - for now
-exports.getTransitRoute = (req, res, next) => {
-    res.status(200).json({success:true, message:"One transit route", route: `${req.params.id}`})
+exports.getTransitRoute = async (req, res, next) => {
+    try {
+        const transitRoute = await TransitRouteSchema.findById(req.params.id)
+        res.status(200).json({success:true, data: transitRoute})
+    } catch (error) {
+        res.status(400)
+    }
 }
 
 // @desc Add a transit routes
 // @route POST /api/v1/transit-routes/
 // @access Admin
-exports.addTransitRoute = (req, res, next) => {
-    res.status(201).json({success:true, message:"New resource created",route:`${res.body}`})
+exports.addTransitRoute = async (req, res, next) => {
+    try {
+        const addRoute = await TransitRouteSchema.create(req.body)
+        res.status(201).json({success:true, data: addRoute})
+    } catch (error) {
+        res.status(400)
+    }
 }
 
 // @desc Update transit route
 // @route PUT /api/v1/transit-routes/:id
 // @access Admin
-exports.updateTransitRoute = (req, res, next) => {
-    res.status(204).json({success:true, message:`Route updated ${req.params.id}`})
+exports.updateTransitRoute = async (req, res, next) => {
+   try {
+    const updateRoute = await TransitRouteSchema.findByIdAndUpdate(req.params.id, {data:req.body})
+    res.status(200).json({data:updateRoute})
+   } catch (error) {
+    res.status(400)
+   }
 }
 
 // @desc Delete transit routes
 // @route DELETE /api/v1/routes/:id
 // @access Admin
-exports.deleteTransitRoute = (req, res, next) => {
-   res.send('deleted')
+exports.deleteTransitRoute = async (req, res, next) => {
+   try {
+    const deleteRoute = await TransitRouteSchema.findByIdAndDelete(req.params.id)
+    res.status(200).json({success:true, data: deleteRoute})
+   } catch (error) {
+    res.status(400)
+   }
 }
--- schemas/transitRouteSchema.js
+++ schemas/transitRouteSchema.js
@@ -2,18 +2,10 @@ const mongoose = require('mongoose');
 const stopSchema = require('./stopSchema');
 
 const transitRouteSchema = new mongoose.Schema({
-   metadata: {
-    created_at: Date.now(),
-    created_by: String
-   },
+  
     name: String,
     description: String,
-    stops: [stopSchema],
-    schedule: [...schedule],
-    current_location: {
-        coords: Object,
-        timestamp: Date.now()
-    }
+   
 
 })
 
